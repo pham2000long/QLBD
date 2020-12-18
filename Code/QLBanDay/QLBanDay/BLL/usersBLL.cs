366a93fcf3aa3524bd0fa42ee0919bd2aad9791d
HaMa <vanha.jungle@gmail.com>
2020-12-18 21:34:18 +0700

    gd product

---
+++ Code/QLBanDay/QLBanDay/BLL/usersBLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using QLBanDay.DTO;
+
+namespace QLBanDay.BLL
+{
+    class usersBLL
+    {
+        dal dal = new dal();
+       
+        public bool getUsernamPassword(string usename, string password)
+        {
+            string sql = "select * from users where username='"+usename+ "' AND password='"+password+"'";
+            DataTable dt = new DataTable();
+            dt = dal.GetTable(sql);
+            return dt.Rows.Count == 0 ? false : true;
+        }
+
+    }
+}
