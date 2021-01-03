93865014b00ebe1d65a3243d44608e5fb3c8a990
Phạm Long <pham2000long@gmail.com>
2021-01-03 14:04:51 +0700

    ngu lol

--- Code/QLBanDay/QLBanDay/BLL/categoriesBLL.cs
+++ Code/QLBanDay/QLBanDay/BLL/categoriesBLL.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using QLBanDay.DTO;
 
 namespace QLBanDay.BLL
 {
-    class productsBLL
+    class categoriesBLL
     {
         dal dal = new dal();
-
+        
     }
 }
--- Code/QLBanDay/QLBanDay/DTO/ProductsDTO.cs
+++ Code/QLBanDay/QLBanDay/DTO/ProductsDTO.cs
@@ -8,5 +8,20 @@ namespace QLBanDay.DTO
 {
     class ProductsDTO
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public int Price { get; set; }
+        public int Size { get; set; }
+        public string Brand { get; set; }
+        public int Color { get; set; }
+        public bool Gender { get; set; }
+        public string Material { get; set; }
+        public int Amount { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public int Category_id { get; set; }
     }
 }
